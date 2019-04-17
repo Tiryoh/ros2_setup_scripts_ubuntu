47093c62b8ff716c4aaa4b491f86d7478a49ddde
Tiryoh <tiryoh@gmail.com>
2019-04-17 19:52:23 +0900

    Update for auto-install

--- run.sh
+++ run.sh
@@ -6,19 +6,23 @@ set -eu
 CHOOSE_ROS_DISTRO=crystal # or bouncy or ardent
 INSTALL_PACKAGE=desktop # or ros-base
 
-sudo apt update -qq
-sudo apt install curl gnupg2 lsb-release
+sudo apt update
+sudo apt install -y curl gnupg2 lsb-release
 curl http://repo.ros2.org/repos.key | sudo apt-key add -
 sudo sh -c 'echo "deb [arch=amd64,arm64] http://packages.ros.org/ros2/ubuntu $(lsb_release -cs) main" > /etc/apt/sources.list.d/ros2-latest.list'
-sudo apt update -qq
-sudo apt install ros-$CHOOSE_ROS_DISTRO-$INSTALL_PACKAGE
-if [ $(lsb_release -cs) -eq "bionic" ]; then
-	sudo apt install python3-argcomplete
-fi
-if [ $(lsb_release -cs) -eq "xenial" ]; then
-	sudo apt install python3-pip
+sudo apt update
+sudo apt install -y ros-$CHOOSE_ROS_DISTRO-$INSTALL_PACKAGE
+if [ "$(lsb_release -cs)" = "bionic" ]; then
+	sudo apt install -y python3-argcomplete
+elif [ "$(lsb_release -cs)" = "xenial" ]; then
+	sudo apt install -y python3-pip
 	sudo pip3 install argcomplete
 fi
-source /opt/ros/$CHOOSE_ROS_DISTRO/setup.bash
 grep -F "source /opt/ros/$CHOOSE_ROS_DISTRO/setup.bash" ~/.bashrc ||
 echo "source /opt/ros/$CHOOSE_ROS_DISTRO/setup.bash" >> ~/.bashrc
+
+set +u
+
+source /opt/ros/$CHOOSE_ROS_DISTRO/setup.bash
+
+echo "success installing ROS2 $CHOOSE_ROS_DISTRO"
