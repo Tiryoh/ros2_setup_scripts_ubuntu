951d8c3d78450f4f1079242a231d9fc1fa425486
Tiryoh <tiryoh@gmail.com>
2019-05-16 00:39:27 +0900

    Update source file to minimize the affect
    https://answers.ros.org/question/292566/what-is-the-difference-between-local_setupbash-and-setupbash/

--- tutorial.sh
+++ tutorial.sh
@@ -7,6 +7,6 @@ git clone -b crystal https://github.com/ros2/examples src/examples
 source /opt/ros/crystal/setup.bash
 colcon build --symlink-install
 colcon test
-source ~/ros2_example_ws/install/setup.bash
+source ~/ros2_example_ws/install/local_setup.bash
 echo "Run 'ros2 run examples_rclcpp_minimal_publisher publisher_member_function' on another terminal"
 ros2 run examples_rclcpp_minimal_subscriber subscriber_member_function
