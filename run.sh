a98e6f5f1fe211d4844521746123077de914c975
Tiryoh <tiryoh@gmail.com>
2019-06-01 13:27:16 +0900

    Fix gpg error

--- run.sh
+++ run.sh
@@ -10,7 +10,8 @@ INSTALL_PACKAGE=desktop # or ros-base
 
 sudo apt update
 sudo apt install -y curl gnupg2 lsb-release
-curl http://repo.ros2.org/repos.key | sudo apt-key add -
+curl -Ls http://repo.ros2.org/repos.key | sudo apt-key add -
+curl -Ls http://packages.ros.org/ros.key | sudo apt-key add -
 sudo sh -c 'echo "deb [arch=amd64,arm64] http://packages.ros.org/ros2/ubuntu $(lsb_release -cs) main" > /etc/apt/sources.list.d/ros2-latest.list'
 sudo apt update
 sudo apt install -y ros-$CHOOSE_ROS_DISTRO-$INSTALL_PACKAGE
