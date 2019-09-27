319e4be845e73a01538f721230b59793bc490517
Tiryoh <tiryoh@gmail.com>
2019-09-27 15:35:54 +0900

    Update colcon build option

--- tutorial.sh
+++ tutorial.sh
@@ -6,7 +6,7 @@ cd ~/ros2_example_ws
 git clone -b dashing https://github.com/ros2/examples src/examples
 source /opt/ros/dashing/setup.bash
 #colcon build --symlink-install
-MAKEFLAGS="-j1 -l1" colcon build --symlink-install --executor sequential # build one package at once https://github.com/Tiryoh/ros2_setup_scripts_ubuntu/issues/4
+colcon build --symlink-install --parallel-workers 1 # build one package at once https://github.com/Tiryoh/ros2_setup_scripts_ubuntu/issues/4
 colcon test
 source ~/ros2_example_ws/install/local_setup.bash
 echo "Run 'ros2 run examples_rclcpp_minimal_publisher publisher_member_function' on another terminal"
