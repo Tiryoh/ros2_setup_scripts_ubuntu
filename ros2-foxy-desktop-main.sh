29bb5b2ea8c1668cefb1316afbf22c90a055d2e6
Tiryoh <tiryoh@gmail.com>
2020-08-04 20:35:56 +0900

    feat: Add independent installers

---
+++ ros2-foxy-desktop-main.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+set -eu
+
+# REF: https://index.ros.org/doc/ros2/Installation/Linux-Install-Debians/
+# by Open Robotics, licensed under CC-BY-4.0
+# source: https://github.com/ros2/ros2_documentation
+
+[[ "$(lsb_release -sc)" == "foxy" ]] || exit 1
+
+CHOOSE_ROS_DISTRO=foxy
+INSTALL_PACKAGE=desktop
+
+sudo apt-get update
+sudo apt-get install -y curl gnupg2 lsb-release
+curl -Ls https://raw.githubusercontent.com/ros/rosdistro/master/ros.key | sudo apt-key add -
+sudo sh -c 'echo "deb [arch=$(dpkg --print-architecture)] http://packages.ros.org/ros2/ubuntu $(lsb_release -cs) main" > /etc/apt/sources.list.d/ros2-latest.list'
+sudo apt-get update
+sudo apt-get install -y ros-$CHOOSE_ROS_DISTRO-$INSTALL_PACKAGE
+sudo apt-get install -y python3-argcomplete
+sudo apt-get install -y python3-colcon-common-extensions
+sudo apt-get install -y python3-rosdep python3-vcstool # https://index.ros.org/doc/ros2/Installation/Linux-Development-Setup/
+grep -F "source /opt/ros/$CHOOSE_ROS_DISTRO/setup.bash" ~/.bashrc ||
+echo "source /opt/ros/$CHOOSE_ROS_DISTRO/setup.bash" >> ~/.bashrc
+
+set +u
+
+source /opt/ros/$CHOOSE_ROS_DISTRO/setup.bash
+
+echo "success installing ROS2 $CHOOSE_ROS_DISTRO"
+echo "Run 'source /opt/ros/$CHOOSE_ROS_DISTRO/setup.bash'"
