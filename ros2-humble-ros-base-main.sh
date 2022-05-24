0048df25096642a15fc0399a3a507b2c6601d853
Daisuke Sato <tiryoh@gmail.com>
2022-05-24 19:19:10 +0900

    feat: Add ROS 2 Humble (#29)
    * feat: Add ROS 2 Humble install script
    
    * ci: Add ROS 2 Humble installer test

---
+++ ros2-humble-ros-base-main.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+set -eu
+
+# REF: https://docs.ros.org/en/galactic/Installation/Ubuntu-Install-Debians.html
+# by Open Robotics, licensed under CC-BY-4.0
+# source: https://github.com/ros2/ros2_documentation
+
+CHOOSE_ROS_DISTRO=humble
+INSTALL_PACKAGE=ros-base
+
+sudo apt-get update
+sudo apt-get install software-properties-common
+sudo add-apt-repository universe
+sudo apt-get install -y curl gnupg2 lsb-release build-essential
+
+[[ "$(lsb_release -sc)" == "jammy" ]] || exit 1
+
+sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
+sudo apt-get update
+sudo apt-get install -y ros-$CHOOSE_ROS_DISTRO-$INSTALL_PACKAGE
+sudo apt-get install -y python3-argcomplete
+sudo apt-get install -y python3-colcon-common-extensions
+sudo apt-get install -y python3-rosdep python3-vcstool # https://index.ros.org/doc/ros2/Installation/Linux-Development-Setup/
+[ -e /etc/ros/rosdep/sources.list.d/20-default.list ] ||
+sudo rosdep init
+rosdep update
+grep -F "source /opt/ros/$CHOOSE_ROS_DISTRO/setup.bash" ~/.bashrc ||
+echo "source /opt/ros/$CHOOSE_ROS_DISTRO/setup.bash" >> ~/.bashrc
+
+set +u
+
+source /opt/ros/$CHOOSE_ROS_DISTRO/setup.bash
+
+echo "success installing ROS2 $CHOOSE_ROS_DISTRO"
+echo "Run 'source /opt/ros/$CHOOSE_ROS_DISTRO/setup.bash'"
