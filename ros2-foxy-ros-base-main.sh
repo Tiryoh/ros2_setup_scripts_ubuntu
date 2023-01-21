c53d13c386ac83719e96ee77e4adcf689067d05f
Daisuke Sato <tiryoh@gmail.com>
2023-01-21 18:44:25 +0900

    Remove foxy

--- ros2-foxy-ros-base-main.sh
+++ ros2-foxy-ros-base-main.sh
@@ -46,7 +46,7 @@ sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
 sudo apt-get update
 sudo apt-get install -y ros-$CHOOSE_ROS_DISTRO-$INSTALL_PACKAGE
-sudo apt-get install -y python3-argcomplete python3-colcon-clean
+sudo apt-get install -y python3-argcomplete
 sudo apt-get install -y python3-colcon-common-extensions
 sudo apt-get install -y python3-rosdep python3-vcstool # https://index.ros.org/doc/ros2/Installation/Linux-Development-Setup/
 [ -e /etc/ros/rosdep/sources.list.d/20-default.list ] ||
