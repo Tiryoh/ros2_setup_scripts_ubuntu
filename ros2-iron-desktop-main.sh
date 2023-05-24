74aebdcfce5b48352aeb804bcfc5cc45a8bdf502
Daisuke Sato <tiryoh@gmail.com>
2023-05-24 23:46:44 +0900

    feat: Add ROS 2 Iron Irwini install scripts

---
+++ ros2-iron-desktop-main.sh
@@ -0,0 +1,71 @@
+#!/usr/bin/env bash
+set -eu
+
+# Copyright 2019-2023 Tiryoh
+# https://github.com/Tiryoh/ros2_setup_scripts_ubuntu
+# Licensed under the Apache License, Version 2.0
+#
+# REF: https://docs.ros.org/en/galactic/Installation/Ubuntu-Install-Debians.html
+# by Open Robotics, licensed under CC-BY-4.0
+# source: https://github.com/ros2/ros2_documentation
+
+CHOOSE_ROS_DISTRO=iron
+INSTALL_PACKAGE=desktop
+TARGET_OS=jammy
+
+# Check OS version
+if ! which lsb_release > /dev/null ; then
+	sudo apt-get update
+	sudo apt-get install -y curl lsb-release
+fi
+
+if [[ "$(lsb_release -sc)" == "$TARGET_OS" ]]; then
+	echo "OS Check Passed"
+else
+	printf '\033[33m%s\033[m\n' "=================================================="
+	printf '\033[33m%s\033[m\n' "ERROR: This OS (version: $(lsb_release -sc)) is not supported"
+	printf '\033[33m%s\033[m\n' "=================================================="
+	exit 1
+fi
+
+if ! dpkg --print-architecture | grep -q 64; then
+	printf '\033[33m%s\033[m\n' "=================================================="
+	printf '\033[33m%s\033[m\n' "ERROR: This architecture ($(dpkg --print-architecture)) is not supported"
+	printf '\033[33m%s\033[m\n' "See https://www.ros.org/reps/rep-2000.html"
+	printf '\033[33m%s\033[m\n' "=================================================="
+	exit 1
+fi
+
+# systemd and udev-related packages needs to be updated
+# ref: https://github.com/ros2/ros2_documentation/pull/2581
+# ref: https://itsfoss.com/apt-get-upgrade-vs-dist-upgrade/
+# ref: https://penpen-dev.com/blog/upgrade-tigai/
+sudo apt-get update && sudo apt upgrade -y
+
+# Install
+sudo apt-get update
+sudo apt-get install -y software-properties-common
+sudo add-apt-repository -y universe
+sudo apt-get install -y curl gnupg2 lsb-release build-essential
+
+sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
+sudo apt-get update
+sudo apt-get install -y ros-$CHOOSE_ROS_DISTRO-$INSTALL_PACKAGE
+sudo apt-get install -y python3-argcomplete python3-colcon-clean
+sudo apt-get install -y python3-colcon-common-extensions
+sudo apt-get install -y python3-rosdep python3-vcstool # https://index.ros.org/doc/ros2/Installation/Linux-Development-Setup/
+[ -e /etc/ros/rosdep/sources.list.d/20-default.list ] ||
+sudo rosdep init
+rosdep update
+grep -F "source /opt/ros/$CHOOSE_ROS_DISTRO/setup.bash" ~/.bashrc ||
+echo "source /opt/ros/$CHOOSE_ROS_DISTRO/setup.bash" >> ~/.bashrc
+grep -F "source /usr/share/colcon_argcomplete/hook/colcon-argcomplete.bash" ~/.bashrc ||
+echo "source /usr/share/colcon_argcomplete/hook/colcon-argcomplete.bash" >> ~/.bashrc
+
+set +u
+
+source /opt/ros/$CHOOSE_ROS_DISTRO/setup.bash
+
+echo "success installing ROS2 $CHOOSE_ROS_DISTRO"
+echo "Run 'source /opt/ros/$CHOOSE_ROS_DISTRO/setup.bash'"
--- run.sh
+++ run.sh
@@ -9,7 +9,7 @@ set -eu
 # by Open Robotics, licensed under CC-BY-4.0
 # source: https://github.com/ros2/ros2_documentation
 
-CHOOSE_ROS_DISTRO=humble # or foxy, etc...
+CHOOSE_ROS_DISTRO=humble # or foxy, iron, etc...
 INSTALL_PACKAGE=desktop # or ros-base
 
 sudo apt update
