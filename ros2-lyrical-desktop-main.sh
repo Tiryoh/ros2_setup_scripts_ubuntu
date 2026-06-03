9d125bc4ca7df6e1006bf56259c83ded149eade6
Daisuke Sato <tiryoh@gmail.com>
2026-06-04 02:12:30 +0900

    feat: Add ROS 2 Lyrical installers

---
+++ ros2-lyrical-desktop-main.sh
@@ -0,0 +1,69 @@
+#!/usr/bin/env bash
+set -eu
+
+# Copyright 2019-2026 Tiryoh
+# https://github.com/Tiryoh/ros2_setup_scripts_ubuntu
+# Licensed under the Apache License, Version 2.0
+#
+# REF: https://docs.ros.org/en/lyrical/Installation/Ubuntu-Install-Debs.html
+# by Open Robotics, licensed under CC-BY-4.0
+# source: https://github.com/ros2/ros2_documentation
+
+CHOOSE_ROS_DISTRO=lyrical
+INSTALL_PACKAGE=desktop
+TARGET_OS=resolute
+
+# Check OS version
+if ! which lsb_release > /dev/null ; then
+	sudo apt-get update
+	sudo apt-get install -y curl lsb-release
+fi
+
+if [[ "$(lsb_release -sc)" == "$TARGET_OS" ]]; then
+	echo "OS Check Passed"
+else
+	printf '\033[33m%s\033[m\n' "=================================================="
+	printf '\033[33m%s\033[m\n' "ERROR: This OS (version: $(lsb_release -sc)) is not supported"
+	printf '\033[33m%s\033[m\n' "=================================================="
+	exit 1
+fi
+
+ARCH=$(dpkg --print-architecture)
+if [[ "$ARCH" != "amd64" && "$ARCH" != "arm64" ]]; then
+	printf '\033[33m%s\033[m\n' "=================================================="
+	printf '\033[33m%s\033[m\n' "ERROR: This architecture ($ARCH) is not supported"
+	printf '\033[33m%s\033[m\n' "See https://www.ros.org/reps/rep-2000.html"
+	printf '\033[33m%s\033[m\n' "=================================================="
+	exit 1
+fi
+
+# Install
+sudo apt-get update
+sudo apt-get install -y software-properties-common
+sudo add-apt-repository -y universe
+sudo apt-get install -y curl lsb-release build-essential
+
+ROS_APT_SOURCE_VERSION=$(curl -s https://api.github.com/repos/ros-infrastructure/ros-apt-source/releases/latest | grep -F "tag_name" | awk -F'"' '{print $4}')
+curl -L -o /tmp/ros2-apt-source.deb "https://github.com/ros-infrastructure/ros-apt-source/releases/download/${ROS_APT_SOURCE_VERSION}/ros2-apt-source_${ROS_APT_SOURCE_VERSION}.$(. /etc/os-release && echo ${UBUNTU_CODENAME:-${VERSION_CODENAME}})_all.deb"
+sudo dpkg -i /tmp/ros2-apt-source.deb
+sudo apt-get update
+sudo apt-get install -y ros-$CHOOSE_ROS_DISTRO-$INSTALL_PACKAGE
+sudo apt-get install -y python3-argcomplete python3-colcon-clean
+sudo apt-get install -y python3-colcon-common-extensions
+sudo apt-get install -y python3-rosdep python3-vcstool
+[ -e /etc/ros/rosdep/sources.list.d/20-default.list ] ||
+sudo rosdep init
+rosdep update
+grep -F "source /opt/ros/$CHOOSE_ROS_DISTRO/setup.bash" ~/.bashrc ||
+echo "source /opt/ros/$CHOOSE_ROS_DISTRO/setup.bash" >> ~/.bashrc
+# From ROS 2 Iron, it uses ROS_AUTOMATIC_DISCOVERY_RANGE instead of ROS_LOCALHOST_ONLY
+# https://docs.ros.org/en/iron/Releases/Release-Iron-Irwini.html#improved-discovery-options
+grep -F "export ROS_AUTOMATIC_DISCOVERY_RANGE=" ~/.bashrc ||
+echo "# export ROS_AUTOMATIC_DISCOVERY_RANGE=LOCALHOST" >> ~/.bashrc
+
+set +u
+
+source /opt/ros/$CHOOSE_ROS_DISTRO/setup.bash
+
+echo "success installing ROS2 $CHOOSE_ROS_DISTRO"
+echo "Run 'source /opt/ros/$CHOOSE_ROS_DISTRO/setup.bash'"
